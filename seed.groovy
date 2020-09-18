8255675683ab0010fc9016156e334455ffea7926
Sarthak Sharma <sarthaksharma575@gmail.com>
2020-09-18 13:39:39 +0530

    trial job4

--- seed.groovy
+++ seed.groovy
@@ -2,19 +2,84 @@ freeStyleJob('job1-pull-repo') {
   description("pull codes from GitHub")
   scm {
     github('sarthakSharma5/repo_web', 'master')
-  }
+  }/*
   triggers {
     scm("* * * * *")
-  }
+  }*/
   steps {
     shell(''' echo "copying code to workspace"
-íf sudo ls | grep task6
+if sudo ls | grep task6
 then
   echo "dir exists"
 else
   sudo mkdir /task6
 fi
 sudo cp -rvf * /task6
+''')
+  }
+}
+freeStyleJob('job2-deploy-code') {
+  description('launch app over Kubernetes Cluster')
+  triggers {
+    upstream('job1-pull-repo', 'SUCCESS')
+  }
+  steps {
+    shell ('''
+if sudo kubectl get deploy | grep myweb
+then
+    echo "WebApp already running"
+fi
+if sudo ls /root/workspace | grep *.html
+then
+    echo "Launching WebApp"
+    sudo kubectl apply -f htdeploy.txt
+    pod=$(sudo kubectl get pods -l app=myweb -o jsonpath="{.items[0].metadata.name}")
+    sleep 30
+    sudo kubectl cp /root/workspace/*.html $pod:/usr/local/apache2/htdocs
+elif sudo ls /root/workspace | grep *.php
+then
+    echo "Launching WebApp"
+    sudo kubectl apply -f phdeploy.txt
+    pod=$(sudo kubectl get pods -l app=myweb -o jsonpath="{.items[0].metadata.name}")
+    sleep 30
+    sudo kubectl cp /root/workspace/*.php $pod:/var/www/html
+else
+	echo "appropriate file not found"
+fi
+''')
+  }
+}
+freeStyleJob('job3-check-status') {
+  description('check status of the WebApp')
+  triggers {
+    upstream('job2-deploy-code', 'SUCCESS')
+  }
+  steps {
+    shell('''
+status=$(curl -o /dev/null -s -w %{http_code} 192.168.99.100:31180/) #put your ip
+if [ $status == 200 ]
+then
+	echo "OK"
+	exit 0
+else
+	echo "Error"
+    sudo curl --user admin:goloo005 "http://192.168.99.101:8085/job/job4-mail-dev/build?token=sendmail"
+	exit 1
+fi
+''')
+  }
+}
+freeStyleJob('job4-mail-dev') {
+  description('mail developer if error in code')
+  trigger {
+    parameters {
+        stringParam('myParameterName', 'sendmail', 'called if job3 fails')
+    }
+  }
+  steps {
+    shell('''
+# execute bash commands
+date
 ''')
   }
 }
